cba4d982d2aa57415f069fcbbbe45ef56dfd6a3d
leeseungjun <cjstkrak@gmail.com>
2025-11-03 23:10:09 +0900

    [feat(frontend)]: MVP 항목추가

--- frontend/app/logs/page.tsx
+++ frontend/app/logs/page.tsx
@@ -1,10 +1,5 @@
-import Shell from "@/components/layout/Shell";
 import LogViewer from "@/components/features/LogViewer";
 
 export default function Page() {
-  return (
-    <Shell>
-      <LogViewer />
-    </Shell>
-  );
+  return <LogViewer />;
 }
