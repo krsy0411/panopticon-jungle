0dca5f778cee0d06a6407f51af47a4caac034c36
leeseungjun <cjstkrak@gmail.com>
2025-11-04 19:42:39 +0900

    [feat(Notificaiton)]: 간단한 메일발송시스템 작성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { LogModule } from "./logs/logs.module";
 import { MetricsModule } from "./metrics/metrics.module";
+import { NotificationModule } from "./notification/notification.module";
 
 @Module({
-  imports: [LogModule, MetricsModule],
+  imports: [LogModule, MetricsModule, NotificationModule],
   controllers: [AppController],
   providers: [],
 })
