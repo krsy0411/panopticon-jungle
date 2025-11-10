e48bad43931ec222881fdc259c24ae34e182dbcd
leeseungjun <cjstkrak@gmail.com>
2025-11-10 14:45:28 +0900

    [chore(producer)]: prefix 'producer'로 고정(ALB)

--- producerServer/src/main.ts
+++ producerServer/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // prefix 고정
+  app.setGlobalPrefix('producer', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
